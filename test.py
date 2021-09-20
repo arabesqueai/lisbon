3ed3f5471149c2ac763d54f584b09145828c4b9b
tonyyzy <tony.yang@arabesque.com>
2021-09-20 15:59:07 +0100

    Update test to work on both darwin and linux

--- test.py
+++ test.py
@@ -4,27 +4,47 @@ from sklearn import svm
 import lisbon
 import numpy as np
 import time
+import sys
 
 X, y = fetch_data("adult", return_X_y=True, local_cache_dir="./pmlb/")
+# Make sure the input data is C-style row contigueous array (row orientated)
 X = np.ascontiguousarray(X)
 y = np.ascontiguousarray(y)
 
-linearsvc = svm.LinearSVC(loss="hinge", max_iter=10000, random_state=0)
-t = time.time()
-# mem_usage = memory_usage((linearsvc.fit, [X, y]), interval=1)
-linearsvc.fit(X, y)
-print(f"liblinear took {time.time() - t} seconds and {linearsvc.n_iter_} iterations")
-print("last 10 coefficients: ", linearsvc.coef_[:, -10:])
-print("Intercept: ", linearsvc.intercept_)
-# print("Max memory usage: ", max(mem_usage))
 
+# Memory profiler doesn't run on MacOS
+if sys.platform.startswith("darwin"):
+    linearsvc = svm.LinearSVC(loss="hinge", max_iter=1000, random_state=0)
+    t = time.time()
+    linearsvc.fit(X, y)
+    print(
+        f"liblinear took {time.time() - t} seconds and {linearsvc.n_iter_} iterations"
+    )
+    print("last 10 coefficients: ", linearsvc.coef_[:, -10:])
+    print("Intercept: ", linearsvc.intercept_)
 
-svm._base.liblinear = lisbon
+    svm._base.liblinear = lisbon
 
-t = time.time()
-# mem_usage = memory_usage((linearsvc.fit, [X, y]), interval=1)
-linearsvc.fit(X, y)
-print(f"lisbon took {time.time() - t} seconds and {linearsvc.n_iter_} iterations")
-print("last 10 coefficients: ", linearsvc.coef_[:, -10:])
-print("Intercept: ", linearsvc.intercept_)
-# print("Max memory usage: ", max(mem_usage))
+    t = time.time()
+    linearsvc.fit(X, y)
+    print(f"lisbon took {time.time() - t} seconds and {linearsvc.n_iter_} iterations")
+    print("last 10 coefficients: ", linearsvc.coef_[:, -10:])
+    print("Intercept: ", linearsvc.intercept_)
+else:
+    linearsvc = svm.LinearSVC(loss="hinge", max_iter=1000, random_state=0)
+    t = time.time()
+    mem_usage = memory_usage((linearsvc.fit, [X, y]), interval=1)
+    print(
+        f"liblinear took {time.time() - t} seconds and {linearsvc.n_iter_} iterations"
+    )
+    print("last 10 coefficients: ", linearsvc.coef_[:, -10:])
+    print("Intercept: ", linearsvc.intercept_)
+    print("Max memory usage: ", max(mem_usage))
+
+    svm._base.liblinear = lisbon
+
+    t = time.time()
+    mem_usage = memory_usage((linearsvc.fit, [X, y]), interval=1)
+    print(f"lisbon took {time.time() - t} seconds and {linearsvc.n_iter_} iterations")
+    print("last 10 coefficients: ", linearsvc.coef_[:, -10:])
+    print("Intercept: ", linearsvc.intercept_)
