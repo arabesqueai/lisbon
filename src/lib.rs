eb000c2bcbb1f8c9eb0a1f934615adcace09932d
tonyyzy <tony@tony.tc>
2021-09-20 13:51:57 +0100

    Allow no intercept

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@ mod linear;
 use std::convert::TryInto;
 
 use ndarray::Array2;
-use numpy::{IntoPyArray, PyArray1, PyArray2, PyReadonlyArray1, PyReadonlyArray2};
+use numpy::{
+    IntoPyArray, PyArray1, PyArray2, PyReadonlyArray1, PyReadonlyArray2,
+};
 use pyo3::prelude::*;
 
 use crate::linear::{Parameter, Problem};
@@ -37,11 +39,18 @@ fn lisbon(_py: Python, m: &PyModule) -> PyResult<()> {
         _epsilon: f64,
         _sample_weight: PyReadonlyArray1<'py, f64>,
     ) -> PyResult<(&'py PyArray2<f64>, &'py PyArray1<usize>)> {
-        let sparse = dense_to_sparse(&x);
+        let sparse = dense_to_slices(&x);
         let target = y.as_slice().unwrap();
         let l = x.shape()[0];
         let n = x.shape()[1];
-        let prob = Problem::new(l, n, target, sparse, bias);
+        let b = {
+            if bias > 0.0 {
+                bias
+            } else {
+                0.0
+            }
+        };
+        let prob = Problem::new(l, n, target, sparse, b);
         let param = Parameter {
             solver_type: solver_type.try_into().unwrap(),
             eps,
@@ -59,7 +68,7 @@ fn lisbon(_py: Python, m: &PyModule) -> PyResult<()> {
     Ok(())
 }
 
-fn dense_to_sparse<'a>(arr: &'a PyReadonlyArray2<f64>) -> Vec<&'a [f64]> {
+fn dense_to_slices<'a>(arr: &'a PyReadonlyArray2<f64>) -> Vec<&'a [f64]> {
     let n = arr.shape()[1];
     arr.as_slice().unwrap().chunks(n).collect()
 }
