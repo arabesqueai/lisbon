cae297e1cd1aeaaecde185b224b4caf410ab7e73
tonyyzy <tony.yang@arabesque.com>
2021-12-10 02:12:37 +0000

    Update benchmark check script

--- bench.py
+++ bench.py
@@ -12,26 +12,32 @@ y = np.ascontiguousarray(y)
 
 
 def run():
-    linearsvc = svm.LinearSVC(loss="hinge", max_iter=5000, random_state=0)
+    linearsvc = svm.LinearSVC(loss="hinge", max_iter=500, random_state=0)
     t = time.time()
     if not sys.platform.startswith("darwin"):
         mem_usage = memory_usage((linearsvc.fit, [X, y]), interval=1)
     else:
         linearsvc.fit(X, y)
     time_taken = time.time() - t
-    print(f"liblinear took {time_taken} seconds and {linearsvc.n_iter_} iterations")
+    print(f"Run took {time_taken} seconds and {linearsvc.n_iter_} iterations")
     print("last 10 coefficients: ", linearsvc.coef_[:, -10:])
     print("Intercept: ", linearsvc.intercept_)
     if not sys.platform.startswith("darwin"):
         print("Max memory usage: ", max(mem_usage))
-    return time_taken
+    return linearsvc.coef_, linearsvc.intercept_, time_taken
 
 
-liblinear_time = run()
+liblinear_coef, liblinear_intercept, liblinear_time = run()
 
 import lisbon  # noqa
 
-lisbon_time = run()
+lisbon_coef, lisbon_intercept, lisbon_time = run()
+
+if not np.allclose(liblinear_coef, lisbon_coef, rtol=0, atol=1e-15) or not np.allclose(
+    liblinear_intercept, lisbon_intercept, rtol=0, atol=1e-15
+):
+    print("Lisbon result is different from liblinear. There might be an error.")
+    exit(1)
 
 if lisbon_time > liblinear_time:
     print("Lisbon is slower than liblinear.")
