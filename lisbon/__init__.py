f7af1a79ffb0becee176dfb1e60ee4580ab918ad
tonyyzy <tony.yang@arabesque.com>
2021-12-10 02:12:22 +0000

    Update module init

--- lisbon/__init__.py
+++ lisbon/__init__.py
@@ -1,23 +1,32 @@
 try:
-    from sklearn import svm
+    from sklearn import svm as __svm
 except ModuleNotFoundError:
     print("Please install scikit-learn to use lisbon.")
     exit()
 
 from lisbon import lisbon
 
-orig_fit = svm.LinearSVC.fit
+__orig_fit = __svm.LinearSVC.fit
 
 
-def _lift(self, X, y, sample_weight=None):
+def __lift(self, X, y, sample_weight=None):
+    # conditionally swap liblinear out for lisbon if the routine matches
     if (
-        svm._base._get_liblinear_solver_type(
+        __svm._base._get_liblinear_solver_type(
             self.multi_class, self.penalty, self.loss, self.dual
         )
         == 3
     ):
-        svm._base.liblinear = lisbon
-    return orig_fit(self, X, y, sample_weight)
+        __svm._base.liblinear = lisbon
+    else:
+        __svm._base.liblinear = __svm._liblinear
+    fitted = __orig_fit(self, X, y, sample_weight)
+    __svm._base.liblinear = __svm._liblinear
+    return fitted
 
 
-svm.LinearSVC.fit = _lift
+__svm.LinearSVC.fit = __lift
+
+
+def unload():
+    __svm.LinearSVC.fit = __orig_fit
